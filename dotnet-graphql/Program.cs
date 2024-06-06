acd92bc0c6e3e0cddef6962dd187840657825074
Serhii Popovych <popovychsergiy76@gmail.com>
2024-06-06 22:50:37 +0200

    dotnet client

---
+++ dotnet-graphql/Program.cs
@@ -0,0 +1,32 @@
+﻿using GraphQL;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.Newtonsoft;
+
+namespace dotnet_graphql;
+
+class Program
+{
+    const string url = "https://swapi-graphql.netlify.app/.netlify/functions/index";
+
+    static void Main(string[] args)
+    {
+        var client = new GraphQLHttpClient(url, new NewtonsoftJsonSerializer());
+        var request = new GraphQLRequest
+        {
+            Query = @"
+                query AllFilms {
+                allFilms {
+                    films {
+                    id
+                    director
+                    created
+                    title
+                    }
+                }
+                }
+            "
+        };
+        var response = client.SendQueryAsync<dynamic>(request).Result;
+        Console.WriteLine(response.Data);
+    }
+}
